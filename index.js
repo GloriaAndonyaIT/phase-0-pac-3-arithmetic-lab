d3a510b16b347ea97328d4863e8f01b53be08bbc
GloriaAndonyaIT <gloriaandonyaa@gmail.com>
2025-02-28 17:20:43 +0300

    Assignment complete

---
+++ index.js
@@ -0,0 +1,41 @@
+function  add(a, b){
+    return a + b;
+
+}
+console.log(add(1000,1000));
+
+function subtract(a, b){
+    return a -b;
+}
+console.log(subtract(1000,1000));
+
+function multiply(a, b){
+    return a*b;
+}
+console.log(multiply(1000,1000));
+
+function divide(a, b){
+    return a/b;
+}
+console.log(divide*(1000,1000));
+
+function increment(n){
+    return n+1;
+}
+console.log(increment(10000));
+
+function decrement(n){
+    return n-1;
+}
+console.log(decrement(10000));
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+console.log(makeInt(10000));
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+console.log(preserveDecimal(10000));
+
